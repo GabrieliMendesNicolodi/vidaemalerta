fde387742805b8736a447b616787703e4242d6a4
Gabrieli Mendes Nicolodi <gabiprogramadoraweb@gmail.com>
2024-01-13 15:22:32 -0300

    menu toggle

--- js/main.js
+++ js/main.js
@@ -2,8 +2,10 @@ function clickMenu(icon){
     console.log(icon);
     jQuery("nav").toggle('slow');
     if(jQuery(icon).text() == "menu_open"){
-        jQuery(icon).text("menu");
+        jQuery(icon).text("menu").removeClass("closebtn");
+        jQuery("nav").removeClass("areawidth");
     }else{
-        jQuery(icon).text("menu_open");
+        jQuery(icon).text("menu_open").addClass("closebtn");
+        jQuery("nav").addClass("areawidth");
     }
 }

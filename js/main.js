71ac02780612057b57b76893950dd2682c047939
Gabrieli Mendes Nicolodi <gabiprogramadoraweb@gmail.com>
2024-07-23 22:05:31 -0300

    aplicando correções de css

--- js/main.js
+++ js/main.js
@@ -1,6 +1,4 @@
 function clickMenu(icon){
-    console.log(icon);
-    jQuery("nav").toggle('slow');
     if(jQuery(icon).text() == "menu_open"){
         jQuery(icon).text("menu").removeClass("closebtn");
         jQuery("nav").removeClass("areawidth");
